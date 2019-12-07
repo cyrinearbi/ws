e9e76e40fda58016d2d6b67a965bc3307cd64f2f
cyrine larbi <cyrino.arbi21@gmail.com>
2019-12-07 13:06:00 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+/*const ky =require ('ky');
+
+(async () => {
+    const parsed = await ky.post('https://swapi.co/api/poeple/1', {json: {foo: true}}).json();
+
+console.log(parsed);
+//=> `{data: '🦄'}`
+})();*/
+
+console.log('xxxxxxxx');
